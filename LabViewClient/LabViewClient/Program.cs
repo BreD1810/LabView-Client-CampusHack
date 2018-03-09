29b31bd56ce5b2b591279446aae46a13dbecb5f9
Samuel Collins <samuel.collins@live.co.uk>
2018-03-09 23:35:03 +0000

    Wrote simple program to test HTTP POST requests.

--- LabViewClient/LabViewClient/Program.cs
+++ LabViewClient/LabViewClient/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace LabViewClient
 {
@@ -6,7 +10,22 @@ namespace LabViewClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Debug.WriteLine("Client Started.");
+            WebRequest request = WebRequest.Create("http://localhost:8080");
+            ((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
+            request.Method = "POST";
+            request.ContentType = "text";
+            Stream requestStream = request.GetRequestStream();
+            var data = Encoding.UTF8.GetBytes("I'm a Client, and I'm Alive!!!");
+            requestStream.Write(data, 0, data.Length);
+            requestStream.Close();
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(response);
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+            Console.WriteLine("\r\nPress any key to continue...");
+            Console.ReadKey();
+            
         }
     }
 }
