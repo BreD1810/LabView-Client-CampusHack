92db39b5a7a04f8d07e9d89932b4d8d46a6b852d
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-03-10 18:48:55 +0000

    Completely rewritten code to work with new websocket backend Tomcat thing.

--- LabViewClient/LabViewClient/Program.cs
+++ LabViewClient/LabViewClient/Program.cs
@@ -3,33 +3,53 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace LabViewClient
 {
     class Program
     {
-        const string _serverAddress = "localhost";
-        const int _serverPort = 1337;
+        private static object consoleLock = new object();
+        private const int sendChunkSize = 256;
+        private const int receiveChunkSize = 256;
+        private const bool verbose = true;
+        private static readonly TimeSpan delay = TimeSpan.FromMilliseconds(30000);
+        static UTF8Encoding encoder = new UTF8Encoding();
+
+        //const string _serverAddress = "ws://labview.me/LabView/websocketendpoint";
+        //const int _serverPort = 8080;
 
         static string _machineName = Environment.MachineName;
         static void Main(string[] args)
         {
+
+            Connect("ws://labview.me:8080/LabView/websocketendpoint").Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
             while (true)
             {
-                Debug.WriteLine("Client Started.");
-                Debug.WriteLine($"Machine Name: {_machineName}");
-                var tcpClient = new TcpClient(_serverAddress, _serverPort);
-                Debug.WriteLine($"TCP Connection established with {_serverAddress}");
-                byte[] msg = Encoding.UTF8.GetBytes($"<MachineName>{_machineName}\n");
-                tcpClient.Client.Send(msg);
-                Debug.WriteLine("Message Sent...");
-                byte[] responseBuffer = new byte[1024];
-                int bytesRec = tcpClient.Client.Receive(responseBuffer);
-                string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRec);
-                Debug.WriteLine($"Response received: \"{response}\"");
-                Thread.Sleep(60000);
+                //Debug.WriteLine("Client Started.");
+                //Debug.WriteLine($"Machine Name: {_machineName}");
+                ////var tcpClient = new TcpClient(_serverAddress, _serverPort);
+
+                
+
+               
+                
+
+                //Debug.WriteLine($"TCP Connection established with {_serverAddress}");
+                //byte[] msg = Encoding.UTF8.GetBytes($"<MachineName>{_machineName}\n");
+                ////tcpClient.Client.Send(msg);
+                //Debug.WriteLine("Message Sent...");
+                //byte[] responseBuffer = new byte[1024];
+                //int bytesRec = tcpClient.Client.Receive(responseBuffer);
+                //string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRec);
+                //Debug.WriteLine($"Response received: \"{response}\"");
+                //Thread.Sleep(60000);
             }
 
             //while (Console.KeyAvailable)
@@ -37,5 +57,80 @@ namespace LabViewClient
             //Console.WriteLine("\r\nPress any key to continue...");
             //Console.ReadKey();
         }
+
+        public static async Task Connect(string uri)
+        {
+            ClientWebSocket webSocket = null;
+
+            try
+            {
+                webSocket = new ClientWebSocket();
+                await webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+                await Task.WhenAll(Receive(webSocket), Send(webSocket));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex);
+            }
+            finally
+            {
+                if (webSocket != null)
+                    webSocket.Dispose();
+                Console.WriteLine();
+
+                lock (consoleLock)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("WebSocket closed.");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        private static async Task Send(ClientWebSocket webSocket)
+        {
+
+            //byte[] buffer = encoder.GetBytes("{\"op\":\"blocks_sub\"}"); //"{\"op\":\"unconfirmed_sub\"}");
+            byte[] buffer = encoder.GetBytes("{\"op\":\"unconfirmed_sub\"}");
+            await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                LogStatus(false, buffer, buffer.Length);
+                await Task.Delay(delay);
+            }
+        }
+
+        private static async Task Receive(ClientWebSocket webSocket)
+        {
+            byte[] buffer = new byte[receiveChunkSize];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                }
+                else
+                {
+                    LogStatus(true, buffer, result.Count);
+                }
+            }
+        }
+
+        private static void LogStatus(bool receiving, byte[] buffer, int length)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = receiving ? ConsoleColor.Green : ConsoleColor.Gray;
+                //Console.WriteLine("{0} ", receiving ? "Received" : "Sent");
+
+                if (verbose)
+                    Console.WriteLine(encoder.GetString(buffer));
+
+                Console.ResetColor();
+            }
+        }
     }
+
 }
