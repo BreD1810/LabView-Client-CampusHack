106208785290d155811a626d0d4b0e50e6e8ca8c
Samuel Collins <samuel.collins@live.co.uk>
2018-03-10 14:57:12 +0000

    Modified code to use TCPClient socket.

--- LabViewClient/LabViewClient/Program.cs
+++ LabViewClient/LabViewClient/Program.cs
@@ -2,30 +2,44 @@
 using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace LabViewClient
 {
     class Program
     {
+        const string _serverAddress = "http://localhost";
+        const int _serverPort = 8080;
         static void Main(string[] args)
         {
             Debug.WriteLine("Client Started.");
-            WebRequest request = WebRequest.Create("http://localhost:8080");
-            ((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
-            request.Method = "POST";
-            request.ContentType = "text";
-            Stream requestStream = request.GetRequestStream();
-            var data = Encoding.UTF8.GetBytes("I'm a Client, and I'm Alive!!!");
-            requestStream.Write(data, 0, data.Length);
-            requestStream.Close();
-            WebResponse response = request.GetResponse();
-            Console.WriteLine(response);
+            var tcpClient = new TcpClient(_serverAddress, _serverPort);
+            Debug.WriteLine($"TCP Connection established with {_serverAddress}");
+            byte[] msg = Encoding.UTF8.GetBytes("I'm a Client and I'm Alive!!!!!!");
+            tcpClient.Client.Send(msg);
+            Debug.WriteLine("Message Sent...");
+            byte[] responseBuffer = new byte[1024];
+            int bytesRec = tcpClient.Client.Receive(responseBuffer);
+            string response = Encoding.ASCII.GetString(responseBuffer, 0, bytesRec);
+            Debug.WriteLine($"Response received: \"{response}\"");
+
+            //WebRequest request = WebRequest.Create("http://localhost:8080");
+            //((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
+            //request.Method = "POST";
+            //request.ContentType = "text";
+            //Stream requestStream = request.GetRequestStream();
+            //var data = Encoding.UTF8.GetBytes("I'm a Client, and I'm Alive!!!");
+            //requestStream.Write(data, 0, data.Length);
+            //requestStream.Close();
+            //WebResponse response = request.GetResponse();
+            //Console.WriteLine(response);
             while (Console.KeyAvailable)
                 Console.ReadKey(true);
             Console.WriteLine("\r\nPress any key to continue...");
             Console.ReadKey();
-            
+
+
         }
     }
 }
