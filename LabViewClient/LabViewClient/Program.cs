a376108da0fc53c0be7a410145b2c990762044a3
Samuel Collins <samuel.collins@live.co.uk>
2018-03-10 16:57:06 +0000

    Reformatted the Machine Name message in pseudo-XML

--- LabViewClient/LabViewClient/Program.cs
+++ LabViewClient/LabViewClient/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace LabViewClient
 {
@@ -15,24 +16,26 @@ namespace LabViewClient
         static string _machineName = Environment.MachineName;
         static void Main(string[] args)
         {
-            Debug.WriteLine("Client Started.");
-            Debug.WriteLine($"Machine Name: {_machineName}");
-            var tcpClient = new TcpClient(_serverAddress, _serverPort);
-            Debug.WriteLine($"TCP Connection established with {_serverAddress}");
-            byte[] msg = Encoding.UTF8.GetBytes($"{_machineName}\n");
-            tcpClient.Client.Send(msg);
-            Debug.WriteLine("Message Sent...");
-            byte[] responseBuffer = new byte[1024];
-            int bytesRec = tcpClient.Client.Receive(responseBuffer);
-            string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRec);
-            Debug.WriteLine($"Response received: \"{response}\"");
+            while (true)
+            {
+                Debug.WriteLine("Client Started.");
+                Debug.WriteLine($"Machine Name: {_machineName}");
+                var tcpClient = new TcpClient(_serverAddress, _serverPort);
+                Debug.WriteLine($"TCP Connection established with {_serverAddress}");
+                byte[] msg = Encoding.UTF8.GetBytes($"<MachineName>{_machineName}\n");
+                tcpClient.Client.Send(msg);
+                Debug.WriteLine("Message Sent...");
+                byte[] responseBuffer = new byte[1024];
+                int bytesRec = tcpClient.Client.Receive(responseBuffer);
+                string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRec);
+                Debug.WriteLine($"Response received: \"{response}\"");
+                Thread.Sleep(60000);
+            }
 
-            while (Console.KeyAvailable)
-                Console.ReadKey(true);
-            Console.WriteLine("\r\nPress any key to continue...");
-            Console.ReadKey();
-
-
+            //while (Console.KeyAvailable)
+            //    Console.ReadKey(true);
+            //Console.WriteLine("\r\nPress any key to continue...");
+            //Console.ReadKey();
         }
     }
 }
