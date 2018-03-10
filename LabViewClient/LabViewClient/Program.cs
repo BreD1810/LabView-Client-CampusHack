06646a02ca168c69f0e89721202a46fc6323672f
Samuel Collins <samuel.collins@live.co.uk>
2018-03-10 16:52:01 +0000

    Client now sends machine name in initial message.

--- LabViewClient/LabViewClient/Program.cs
+++ LabViewClient/LabViewClient/Program.cs
@@ -11,12 +11,15 @@ namespace LabViewClient
     {
         const string _serverAddress = "localhost";
         const int _serverPort = 1337;
+
+        static string _machineName = Environment.MachineName;
         static void Main(string[] args)
         {
             Debug.WriteLine("Client Started.");
+            Debug.WriteLine($"Machine Name: {_machineName}");
             var tcpClient = new TcpClient(_serverAddress, _serverPort);
             Debug.WriteLine($"TCP Connection established with {_serverAddress}");
-            byte[] msg = Encoding.UTF8.GetBytes("I'm a Client and I'm Alive!!!!!!\n");
+            byte[] msg = Encoding.UTF8.GetBytes($"{_machineName}\n");
             tcpClient.Client.Send(msg);
             Debug.WriteLine("Message Sent...");
             byte[] responseBuffer = new byte[1024];
@@ -24,16 +27,6 @@ namespace LabViewClient
             string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRec);
             Debug.WriteLine($"Response received: \"{response}\"");
 
-            //WebRequest request = WebRequest.Create("http://localhost:8080");
-            //((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
-            //request.Method = "POST";
-            //request.ContentType = "text";
-            //Stream requestStream = request.GetRequestStream();
-            //var data = Encoding.UTF8.GetBytes("I'm a Client, and I'm Alive!!!");
-            //requestStream.Write(data, 0, data.Length);
-            //requestStream.Close();
-            //WebResponse response = request.GetResponse();
-            //Console.WriteLine(response);
             while (Console.KeyAvailable)
                 Console.ReadKey(true);
             Console.WriteLine("\r\nPress any key to continue...");
